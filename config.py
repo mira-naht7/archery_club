ff3ee903ab9e17f5a5293fc2c2e11dff41f3629b
LAPTOP-JAA837JD\shuns <h20197@e.hiroshima-cu.ac.jp>
2024-09-09 17:46:27 +0900

    Add configuration files for Heroku deployment

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
+        'postgresql://shun:ivnsfinfpibnsnfspm@localhost/archery_club_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
