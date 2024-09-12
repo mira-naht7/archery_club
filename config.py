bd2b70830014e3e46dcd6840b7359ffc0312426e
LAPTOP-JAA837JD\shuns <h20197@e.hiroshima-cu.ac.jp>
2024-09-12 20:13:10 +0900

    Update database configuration for multiple environments

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ load_dotenv()  # .envファイルから環境変数を読み込む
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
     DATABASE_URL = os.getenv('DATABASE_URL') or os.getenv('DEV_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or os.getenv('DEV_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
