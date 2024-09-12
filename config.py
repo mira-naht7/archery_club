363c919f4450f5374aa5f82dbf4b0b6f0ecc63a9
LAPTOP-JAA837JD\shuns <h20197@e.hiroshima-cu.ac.jp>
2024-09-12 20:16:45 +0900

    Update database configuration for multiple environments

--- config.py
+++ config.py
@@ -6,5 +6,7 @@ load_dotenv()  # .envファイルから環境変数を読み込む
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
     DATABASE_URL = os.getenv('DATABASE_URL') or os.getenv('DEV_DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or os.getenv('DEV_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or os.getenv('DEV_DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
