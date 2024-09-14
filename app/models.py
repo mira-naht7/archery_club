c877c9747a53871b2bbf70eebd4d032ced1d0cd4
LAPTOP-JAA837JD\shuns <h20197@e.hiroshima-cu.ac.jp>
2024-09-14 16:29:50 +0900

    Change column type to Text

--- app/models.py
+++ app/models.py
@@ -4,15 +4,15 @@ from app import db, login_manager
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    username = db.Column(db.Text, index=True, unique=True) 
+    password_hash = db.Column(db.Text) 
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
+    
 class Equipment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
